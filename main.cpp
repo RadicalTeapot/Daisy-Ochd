03d616612682775132bbfb9f9917a7693a547060
Mathias Capdet <mathias.capdet@tescan.com>
2023-01-17 16:14:12 +0100

    Add OLED screen support

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
+#include <stdio.h>
 #include "daisy_seed.h"
 #include "daisysp.h"
+#include "dev/oled_ssd130x.h"
 
 using namespace daisy;
 using namespace daisysp;
@@ -12,10 +14,11 @@ using namespace daisysp;
  * A1           Rate CV in                                      5V tolerant
  * A2           Attenuverter in     (not implemented yet)       3.3V tolerant
  * A2           Waveform switch     (not implemented yet)       3.3V tolerant
+ * D11          OLED SCL                                        I2C1 SCL
+ * D12          OLED SDA                                        I2C1 SDA
  *
 */
 
-// TODO Write code to show LFOs on OLED screen
 // TODO Implement attenuverter
 // TODO Implement waveform switch
 
@@ -34,6 +37,9 @@ const float kLfoFreqRatios[kOutputCount] = {1.0f, 0.551f, 0.271f, 0.16f, 0.084f,
 
 float lfoValues[kOutputCount];
 size_t blockIndex;
+
+using Display = OledDisplay<SSD130xI2c128x64Driver>;
+Display display;
 
 void AudioCallback(AudioHandle::InterleavingInputBuffer in,
                     AudioHandle::InterleavingOutputBuffer out,
@@ -73,12 +79,19 @@ int main(void)
     for (size_t i = 0; i < kOutputCount; i++)
         hwPin[i].Init(kPins[i], GPIO::Mode::OUTPUT, GPIO::Pull::NOPULL, GPIO::Speed::VERY_HIGH);
 
+    // Configure display
+    // Leave to default (used pins are I2C1 SCL and I2C1 SDA)
+    Display::Config cfg;
+    display.Init(cfg);
+
     hw.StartAudio(AudioCallback);
 
     size_t pwmThresholds[kOutputCount];
     size_t i, j;
     uint32_t now;
-    uint32_t timeSinceLast = 0;
+    uint32_t timeSinceLastAdcRead = 0;
+    uint32_t timeSinceLastScreenDraw = 0;
+    float adcValue, mappedAdcValue = 0;
     for (;;)
 	{
         for (i = 0; i < kOutputCount; i++)
@@ -91,12 +104,43 @@ int main(void)
         }
 
         now = System::GetNow();
-        if (now > timeSinceLast) {
-            timeSinceLast = now + 10;
-            float adcValue = fclamp(hw.adc.GetFloat(0) + hw.adc.GetFloat(1), 0, 1);
-            float mapped = fmap(adcValue, kLfoMinFreq, kLfoMaxFreq);
+        if (now > timeSinceLastAdcRead) {
+            timeSinceLastAdcRead = now + 10;
+            adcValue = fclamp(hw.adc.GetFloat(0) + hw.adc.GetFloat(1), 0, 1);
+            mappedAdcValue = fmap(adcValue, kLfoMinFreq, kLfoMaxFreq);
             for (i = 0; i < kOutputCount; i++)
-                lfo[i].SetFreq(mapped * kLfoFreqRatios[i]);
+                lfo[i].SetFreq(mappedAdcValue * kLfoFreqRatios[i]);
+        }
+
+        if (now > timeSinceLastScreenDraw) {
+            timeSinceLastScreenDraw = now + 40; // 25 FPS
+
+            display.Fill(false);
+            display.DrawRect(0,0,127,63,true,false);
+            display.DrawLine(0, 32, 123, 32, true);
+            display.DrawLine(32, 0, 32, 63, true);
+            display.DrawLine(63, 0, 63, 63, true);
+            display.DrawLine(94, 0, 94, 63, true);
+
+            uint_fast32_t height = 2 + static_cast<uint_fast32_t>((1 - (lfoValues[0] + 0.5f))*29);
+            display.DrawRect(7,height,27,30,true,true);
+            height = 2 + static_cast<uint_fast32_t>((1 - (lfoValues[1] + 0.5f))*29);
+            display.DrawRect(38,height,58,30,true,true);
+            height = 2 + static_cast<uint_fast32_t>((1 - (lfoValues[2] + 0.5f))*29);
+            display.DrawRect(69,height,89,30,true,true);
+            height = 2 + static_cast<uint_fast32_t>((1 - (lfoValues[3] + 0.5f))*29);
+            display.DrawRect(100,height,120,30,true,true);
+
+            height = 34 + static_cast<uint_fast32_t>((1 - (lfoValues[4] + 0.5f))*29);
+            display.DrawRect(7,height,27,61,true,true);
+            height = 34 + static_cast<uint_fast32_t>((1 - (lfoValues[5] + 0.5f))*29);
+            display.DrawRect(38,height,58,61,true,true);
+            height = 34 + static_cast<uint_fast32_t>((1 - (lfoValues[6] + 0.5f))*29);
+            display.DrawRect(69,height,89,61,true,true);
+            height = 34 + static_cast<uint_fast32_t>((1 - (lfoValues[7] + 0.5f))*29);
+            display.DrawRect(100,height,120,61,true,true);
+
+            display.Update();
         }
 	}
 }
